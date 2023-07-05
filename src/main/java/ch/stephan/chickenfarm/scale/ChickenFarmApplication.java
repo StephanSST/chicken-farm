e3ba52e1365b85515cefa9bf6cb24740f6699438
stephan <stephan.staeheli@gmx.ch>
2023-07-05 07:55:19 +0200

    initial add

---
+++ src/main/java/ch/stephan/chickenfarm/scale/ChickenFarmApplication.java
@@ -0,0 +1,13 @@
+package ch.stephan.chickenfarm.scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenFarmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ch/stephan/chickenfarm/scale/Greeting.java
@@ -0,0 +1,3 @@
+package ch.stephan.chickenfarm.scale;
+
+public record Greeting(long id, String content) { }
