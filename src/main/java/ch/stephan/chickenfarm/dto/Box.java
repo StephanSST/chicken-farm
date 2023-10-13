9193b16a887fbbffab84255513c190e2a580716e
stephan <stephan.staeheli@gmx.ch>
2023-10-13 15:21:42 +0200

    constants for chicken and box

--- src/main/java/ch/stephan/chickenfarm/dto/Box.java
+++ src/main/java/ch/stephan/chickenfarm/dto/Box.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class Box {
+    public static final Box VORNE = new Box("ZUw", "vorne", BoxState.EMPTY);
+    public static final Box HINTEN = new Box("23yp", "hinten", BoxState.EMPTY);
 
 	private String id;
 	private String description;
--- src/main/java/ch/stephan/chickenfarm/dto/Chicken.java
+++ src/main/java/ch/stephan/chickenfarm/dto/Chicken.java
@@ -1,4 +1,7 @@
 package ch.stephan.chickenfarm.dto;
 
 public record Chicken(String name, int weight) {
+    public static final Chicken HEIDI = new Chicken("Heidi", 2000);
+    public static final Chicken KLARA = new Chicken("Klara", 2850);
+    public static final Chicken LILI = new Chicken("Lili", 2750);
 }
